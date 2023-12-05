5e9058442dc83f05be79a8061af3eaddaaec21c3
IvanSpallaUgalde <ivanuib332@gmail.com>
2023-12-05 15:48:02 +0100

    Deleted bot Token

--- Example_bot.py
+++ Example_bot.py
@@ -1,7 +1,7 @@
 import discord
 from discord.ext import commands
 
-BOT_TOKEN = 'MTE3OTgxNDk5Nzc5OTQ4OTU0Ng.GDlqG-.M-vU4Xw-GxoW5VpK69QeXGXG-YJSdevTvkkIh8'
+BOT_TOKEN = ''
 CHANNEL_ID = 1180127744567279727
 
 bot = commands.Bot(command_prefix='!', intents=discord.Intents.all())
